e1e28b7b1732f932d0a050824b6effe244959f10
gunj13 <121148154+gunj13@users.noreply.github.com>
2025-02-09 04:10:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_headers=["*"],
+    expose_headers=["*"],# Allows all headers
 )
 
 @app.get("/")
